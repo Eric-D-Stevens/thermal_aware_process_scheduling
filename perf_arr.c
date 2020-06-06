b6c1a06c15eddd57b928ec2448459b5ade67be47
eric-d-stevens <ericstevens26101@gmail.com>
2020-06-05 18:26:11 -0700

    exit on 80

--- perf_arr.c
+++ perf_arr.c
@@ -13,16 +13,19 @@
 #define N 5 // number of hw events to monitor
 #define M 2 // number of hw events to monitor
 
+// setup to abort main process when child process returns
 int global_sigchld_trip = 0; // catch end child
-void sighandler(int);
+void sighandler(int signum) {
+   printf("Caught signal %d, coming out...\n", signum);
+   global_sigchld_trip = 1;
+}
 
 // function to add perf event to event list
 // based on example from:
 //     http://web.eece.maine.edu/~vweaver/projects/perf_events/perf_event_open.html
 static long
 perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
-					int cpu, int group_fd, unsigned long flags)
-{
+					int cpu, int group_fd, unsigned long flags){
 	int ret;
 	ret = syscall(__NR_perf_event_open, hw_event, 
                     pid, cpu, group_fd, flags);
@@ -30,25 +33,29 @@ perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
 }
 
 // no function for seeing if we can monitor kernel events
-int no_function(int seconds)
-{
+int no_function(int seconds){
     //sleep(seconds);    
     printf("\n\n Start stress \n\n");
-    system("stress -d 4 -t 120");
+    system("stress -d 20 -c 4 -t 30");
     return 0;
 }
 
 
 
-int
-main(int argc, char **argv){
+int main(int argc, char **argv){
 
-    FILE *temp, *freq, *output;
-    char temp_buff[10];
-    char freq_buff[20];
-
+    // setup bail on child return
     signal(SIGCHLD, sighandler);
 
+    // Setup for reading system information
+    FILE *temp, *freq; // linux virtual files
+    char temp_buff[10]; // reading temp
+    char freq_buff[20]; // reading proc freq
+
+    // output csv file to store collected data
+    FILE *output;
+
+    // declare hardware events that can be monitored
 	int num_hw_events = N;
 	uint pe_hw[7] = {
                         PERF_COUNT_HW_CPU_CYCLES,
@@ -73,25 +80,24 @@ main(int argc, char **argv){
                            };
                            
 
-    // cache events
+    // declare hardware cache events that can be monitored
  	int num_hw_cache_events = M;
 	uint pe_hw_cache[12] = {
                     (PERF_COUNT_HW_CACHE_LL) | (PERF_COUNT_HW_CACHE_OP_READ << 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16),
-                    (PERF_COUNT_HW_CACHE_LL) | (PERF_COUNT_HW_CACHE_OP_WRITE<< 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16),
+                    (PERF_COUNT_HW_CACHE_L1D) | (PERF_COUNT_HW_CACHE_OP_READ << 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16), 
                     //----------------------------------------------------//
-                    (PERF_COUNT_HW_CACHE_L1D) | (PERF_COUNT_HW_CACHE_OP_READ << 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16), // 
+                    (PERF_COUNT_HW_CACHE_LL) | (PERF_COUNT_HW_CACHE_OP_WRITE<< 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16),
                     (PERF_COUNT_HW_CACHE_L1I) | (PERF_COUNT_HW_CACHE_OP_READ << 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16),
                     (PERF_COUNT_HW_CACHE_BPU) | (PERF_COUNT_HW_CACHE_OP_READ << 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16),
-
-                    (PERF_COUNT_HW_CACHE_L1D) | (PERF_COUNT_HW_CACHE_OP_WRITE<< 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16), // 
+                    (PERF_COUNT_HW_CACHE_L1D) | (PERF_COUNT_HW_CACHE_OP_WRITE<< 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16),
                     (PERF_COUNT_HW_CACHE_BPU) | (PERF_COUNT_HW_CACHE_OP_WRITE<< 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16),
                     };
 	
     char hw_cache_name_arr[12][50] = {
                     "PERF_COUNT_HW_CACHE_LL_read_miss",
-                    "PERF_COUNT_HW_CACHE_LL_write_miss",
-                    //-------------------------------//
                     "PERF_COUNT_HW_CACHE_L1D_read_miss",
+                    //-------------------------------//
+                    "PERF_COUNT_HW_CACHE_LL_write_miss",
                     "PERF_COUNT_HW_CACHE_L1I_read_miss",
                     "PERF_COUNT_HW_CACHE_BPU_read_miss",
 
@@ -99,10 +105,13 @@ main(int argc, char **argv){
                     "PERF_COUNT_HW_CACHE_BPU_write_miss",
                     };
 
-
+    // hols all perf events to be added to the event list
 	struct perf_event_attr pat_arr[num_hw_events+num_hw_cache_events];
 
+    // holds return from counter values
 	long long counts[num_hw_events+num_hw_cache_events];
+
+    // file descriptors that point to the individual events
 	int fd_arr[num_hw_events+num_hw_cache_events];
 
 
@@ -167,7 +176,6 @@ main(int argc, char **argv){
         printf("entered child process\n");
         int no_sleep = 1;
         no_function(no_sleep);
-        //x = silly_events(loop);
         printf("exiting child process\n");
         return 0;
     }
@@ -177,7 +185,7 @@ main(int argc, char **argv){
         // output file header
         printf("\n\n\nWRITING HEADER\n\n\n");
         output = fopen("output.txt","w+");
-        fprintf(output, "tempature,");
+        fprintf(output, "temp,");
         for(int i=0;i<num_hw_events;i++){
             fprintf(output, hw_name_arr[i]);
             if(i+1==num_hw_events+num_hw_cache_events){break;}
@@ -192,6 +200,7 @@ main(int argc, char **argv){
         fclose(output);
         
 
+        int loop_counter = 0;
         while(!global_sigchld_trip){
             sleep(1);
             temp = fopen("/sys/class/thermal/thermal_zone0/temp","r");
@@ -200,7 +209,13 @@ main(int argc, char **argv){
             for(int i=0;i<num_hw_events+num_hw_cache_events;i++){ioctl(fd_arr[i], PERF_EVENT_IOC_RESET,0);}
             fscanf(temp, "%s", temp_buff);
             fscanf(freq, "%s", freq_buff);
-            printf("\nerror below here\n");
+            int cur_temp = atoi(temp_buff);
+            if(cur_temp > 80000){
+                printf("\n\nEXIT TEMP: %d\n", cur_temp);
+                printf("\n\nRUNTIME: %d seconds\n\n", cur_temp);
+                kill(0, SIGINT);
+                exit(0);
+            }
             fclose(temp);
             fclose(freq);
             //fgets(buff, 255, (FILE*)fp);
@@ -222,6 +237,7 @@ main(int argc, char **argv){
             }
             fprintf(output,"\n");
             fclose(output);
+            loop_counter++;
 
         }
 
@@ -236,7 +252,3 @@ main(int argc, char **argv){
 	return 0;
 }
 
-void sighandler(int signum) {
-   printf("Caught signal %d, coming out...\n", signum);
-   global_sigchld_trip = 1;
-}
