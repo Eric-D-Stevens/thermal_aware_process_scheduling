df1d7973447ffd49d7d9016df05ae918d9098533
eric-d-stevens <ericstevens26101@gmail.com>
2020-06-06 01:23:41 -0700

    proper timeing and temp at start

--- perf_arr.c
+++ perf_arr.c
@@ -9,6 +9,8 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/resource.h>
+#include <sys/time.h>
 
 #define N 5 // number of hw events to monitor
 #define M 2 // number of hw events to monitor
@@ -33,16 +35,37 @@ perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
 }
 
 // no function for seeing if we can monitor kernel events
-int no_function(int seconds){
+int no_function(char *stress_call){
     //sleep(seconds);    
-    printf("\n\n Start stress \n\n");
-    system("stress -d 20 -c 4 -t 30");
+    printf("\n\n Start stress: ");
+    system(stress_call);
+    printf("\n\n");
     return 0;
 }
 
 
 
-int main(int argc, char **argv){
+int main(int argc, char *argv[]){
+    
+    printf("\n NUM ARGS: %d\n\n", argc);
+    char stress_call[60];
+    char out_name[60];
+    char out_filename[60];
+    stress_call[0] = '\0';
+    out_name[0] = '\0';
+    for(int i=1; i<argc; i++){
+        //printf("\n%s", argv[i]);
+        strcat(stress_call, argv[i]);
+        strcat(stress_call, " ");
+        strcat(out_name, argv[i]); 
+    }
+    strcat(out_name, ".csv"); 
+    printf("\n\n\nCALL: %s", stress_call);
+    printf("\nFNAME: %s\n\n", out_name);
+    sprintf(out_filename, "data/%s", out_name);
+    printf("\n\nOFN: %s\n\n",out_filename);
+    //exit(0);
+
 
     // setup bail on child return
     signal(SIGCHLD, sighandler);
@@ -161,7 +184,12 @@ int main(int argc, char **argv){
 	}
 
 
+    double running_time = 0;
+    double t_delta_s, t_delta_u, time_delta;
+    struct timeval start_timer, end_timer;
+    
 	// reset and enable counters
+    gettimeofday(&start_timer, NULL);
 	for(int i=0; i<num_hw_events+num_hw_cache_events; i++){
 		ioctl(fd_arr[i], PERF_EVENT_IOC_RESET,0);
 		ioctl(fd_arr[i], PERF_EVENT_IOC_ENABLE,0);
@@ -170,22 +198,23 @@ int main(int argc, char **argv){
 /////////////////// ACTION ///////////////////////////
 
     /*-------- CHILD PROCESS BEING REDORDED -------*/
-    printf("\nSHOULD FORK RIGHTE HERE\n");
     pid_t proc = fork();
     if(proc==0){
-        printf("entered child process\n");
+        printf("ENTERED CHILD PROCESS\n");
         int no_sleep = 1;
-        no_function(no_sleep);
+        no_function(stress_call);
         printf("exiting child process\n");
         return 0;
     }
     /*-------- ACTION TAKEN DURRING REDORDING-------*/
     else{
 
+
         // output file header
         printf("\n\n\nWRITING HEADER\n\n\n");
-        output = fopen("output.txt","w+");
+        output = fopen(out_filename,"w+");
         fprintf(output, "temp,");
+        fprintf(output, "delta_t,");
         for(int i=0;i<num_hw_events;i++){
             fprintf(output, hw_name_arr[i]);
             if(i+1==num_hw_events+num_hw_cache_events){break;}
@@ -201,23 +230,43 @@ int main(int argc, char **argv){
         
 
         int loop_counter = 0;
+
         while(!global_sigchld_trip){
-            sleep(1);
+            
+            // get starting temp and freq
+            // open virtual files for sys info
             temp = fopen("/sys/class/thermal/thermal_zone0/temp","r");
             freq = fopen("/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq","r");
-            for(int i=0;i<num_hw_events+num_hw_cache_events;i++){read(fd_arr[i], &counts[i], sizeof(long long));}
-            for(int i=0;i<num_hw_events+num_hw_cache_events;i++){ioctl(fd_arr[i], PERF_EVENT_IOC_RESET,0);}
             fscanf(temp, "%s", temp_buff);
             fscanf(freq, "%s", freq_buff);
+            fclose(temp);
+            fclose(freq);
+
+            // give one second to collect events
+            sleep(1);
+
+
+            // read the counters
+            for(int i=0;i<num_hw_events+num_hw_cache_events;i++){read(fd_arr[i], &counts[i], sizeof(long long));}
+
+            // runtime information
+            gettimeofday(&end_timer, NULL);
+            t_delta_s = ((double) (end_timer.tv_sec-start_timer.tv_sec)); // CLOCKS_PER_SEC;
+            t_delta_u = ((double) (end_timer.tv_usec-start_timer.tv_usec))*1e-6; // CLOCKS_PER_SEC;
+            time_delta = t_delta_s + t_delta_u;
+            running_time = running_time + time_delta;
+
+            // reset counters and start timer
+            for(int i=0;i<num_hw_events+num_hw_cache_events;i++){ioctl(fd_arr[i], PERF_EVENT_IOC_RESET,0);}
+            gettimeofday(&start_timer, NULL);
+
             int cur_temp = atoi(temp_buff);
             if(cur_temp > 80000){
                 printf("\n\nEXIT TEMP: %d\n", cur_temp);
-                printf("\n\nRUNTIME: %d seconds\n\n", cur_temp);
+                printf("\nRUNTIME: %f seconds\n\n", running_time);
                 kill(0, SIGINT);
                 exit(0);
             }
-            fclose(temp);
-            fclose(freq);
             //fgets(buff, 255, (FILE*)fp);
             for(int i=0;i<num_hw_events;i++){printf("%lld %s\t\n", counts[i], hw_name_arr[i]);}
             printf("--------------------------------------\n");
@@ -225,11 +274,13 @@ int main(int argc, char **argv){
             printf("--------------------------------------\n");
             printf("temperature: %s\n", temp_buff);
             printf("frequency: %s\n", freq_buff);
+            printf("time delta: %f\n", time_delta);
             printf("\n\n");
 
             // write out line
-            output = fopen("output.txt","a");
+            output = fopen(out_filename,"a");
             fprintf(output,"%s,",temp_buff);
+            fprintf(output,"%ld,",time_delta);
             for(int i=0; i<num_hw_events+num_hw_cache_events; i++){
                 fprintf(output, "%lld", counts[i]);
                 if(i+1==num_hw_events+num_hw_cache_events){break;}
@@ -249,6 +300,7 @@ int main(int argc, char **argv){
 	for(int i=0;i<num_hw_events;i++){printf("Used %lld %s\t", counts[i], hw_name_arr[i]);}
 	for(int i=0;i<num_hw_cache_events;i++){printf("Used %lld %s\t", counts[i+num_hw_events], hw_cache_name_arr[i]);}
 	for(int i=0;i<num_hw_events;i++){close(fd_arr[i]);}
+    printf("\nRUNTIME: %f seconds\n\n", running_time);
 	return 0;
 }
 
--- temp.py
+++
@@ -1,8 +0,0 @@
-import os
-import time
-
-while(1):
-    os.system("vcgencmd measure_temp")
-    os.system("vcgencmd measure_clock arm")
-
-    time.sleep(1)
